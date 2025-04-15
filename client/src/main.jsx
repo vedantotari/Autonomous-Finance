b6e28447383540a573131afe537a5f38392bbb18
vedantotari <165068365+vedantotari@users.noreply.github.com>
2025-04-16 00:10:36 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import {Toaster} from "react-hot-toast"
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { Toaster } from 'react-hot-toast';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Toaster className="toast" position="bottom-left"/>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter basename="/">
+      <Toaster className="toast" position="bottom-left" />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
