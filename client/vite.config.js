c550bfda42481070f6784d0a0be0622a99e392c1
vedantotari <165068365+vedantotari@users.noreply.github.com>
2025-04-15 21:20:31 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Set the base path if you're hosting at the root (adjust if deploying to a subdirectory)
-  build: {
-    outDir: 'dist', // Ensure that the build output goes to the 'dist' directory
-  },
-  server: {
-    open: true, // Automatically open the app in the browser when the dev server starts
-  },
-});
-
+})
